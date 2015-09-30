2b6089400cee78f0c347b7c75127e034ef2d1b48
Andres Montalban <amontalban@globalitss.com>
2015-09-29 21:30:41 -0300

    [MOD] Change package installation to addons.apt.packages as we are using container based infrastructure in Travis-CI

--- _scripts/init.sh
+++ _scripts/init.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/env bash
 set -xe
 
-apt-get -o Acquire::ForceIPv4=true update
-apt-get -o Acquire::ForceIPv4=true -y install jpegoptim optipng
 find . -type f \( -name "*.jpeg" -or -name "*.jpg" \) \
 -exec jpegoptim --strip-all {} \;
 find . -type f -name "*.png" -exec optipng {} \;
