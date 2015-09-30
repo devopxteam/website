3f5cfcca83de01530bfce762a4c61e9bff70374a
Andres Montalban <amontalban@globalitss.com>
2015-09-30 20:30:49 -0300

    [DEL] Remove jpegtran usage

--- _scripts/init.sh
+++ _scripts/init.sh
@@ -3,6 +3,4 @@ set -xe
 
 find . -type f \( -name "*.jpeg" -or -name "*.jpg" \) \
 -exec jpegoptim --strip-all {} \;
-find . -type f \( -name "*.jpeg" -or -name "*.jpg" \) \
--exec jpegtran -copy none -optimize -outfile {} {} \;
 find . -type f -name "*.png" -exec optipng {} \;
