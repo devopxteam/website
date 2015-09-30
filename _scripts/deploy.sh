420ae43fc01c1529b9c9c69c371bbf920445fa8e
Andres Montalban <amontalban@globalitss.com>
2015-09-30 19:44:20 -0300

    [MOD] Add the 'Expires' header to all generated files

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,8 +3,34 @@ set -xe
 
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
   bundle exec rake publish
+  s3_bucket=$(cat s3_website.yml | grep -i s3_bucket | cut -d" " -f 2)
+
+  cd _site
+
+  files=$(find . -type f -print)
+
+  for file in ${files}; do
+    modification_time=$(stat -c '%y' ${file})
+    expires_date=$(date -u -d "${modification_time} +1 year" '+%a, %d %b %Y %H:%M:%S GMT')
+    s3cmd --access_key=${AWS_KEY_ID} --secret_key=${AWS_SECRET_KEY} \
+    --add-header='Expires: ${expires_date}' \
+    _site/ s3://${s3_bucket}/
+  done
 elif [[ $TRAVIS_BRANCH == 'stage' ]] ; then
   bundle exec rake stage
+  s3_bucket=$(cat _stage/s3_website.yml | grep -i s3_bucket | cut -d" " -f 2)
+
+  cd _site
+
+  files=$(find . -type f -print)
+
+  for file in ${files}; do
+    modification_time=$(stat -c '%y' ${file})
+    expires_date=$(date -u -d "${modification_time} +1 year" '+%a, %d %b %Y %H:%M:%S GMT')
+    s3cmd --access_key=${AWS_KEY_ID} --secret_key=${AWS_SECRET_KEY} \
+    --add-header='Expires: ${expires_date}' \
+    _site/ s3://${s3_bucket}/
+  done
 else
   echo 'Invalid branch. You can only deploy from master and stage.'
   exit 1
