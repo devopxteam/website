4dd524e4c73ad3fdd424338be305e03e638a4531
Andres Montalban <amontalban@globalitss.com>
2015-09-30 22:09:12 -0300

    [MOD] Simplify S3 Expires metadata update, changing AWS keys once again

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,34 +2,18 @@
 
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
   bundle exec rake publish
+
   s3_bucket=$(cat s3_website.yml | grep -i s3_bucket | cut -d" " -f 2)
-
-  cd _site
-
-  files=$(find . -type f -print)
-
-  for file in ${files}; do
-    modification_time=$(stat -c '%y' ${file})
-    expires_date=$(date -u -d "${modification_time} +1 year" '+%a, %d %b %Y %H:%M:%S GMT')
-    s3cmd --access_key=${AWS_KEY_ID} --secret_key=${AWS_SECRET_KEY} \
-    --add-header='Expires: ${expires_date}' \
-    _site/ s3://${s3_bucket}/
-  done
+  expires=$(date '+%a, %d %b %Y %H:%M:%S GMT' -d '+1 year')
+  s3cmd --access_key=${AWS_KEY_ID} --secret_key=${AWS_SECRET_KEY} \
+  --recursive modify --add-header="Expires: ${expires}" s3://${s3_bucket}/
 elif [[ $TRAVIS_BRANCH == 'stage' ]] ; then
   bundle exec rake stage
-  s3_bucket=$(cat _stage/s3_website.yml | grep -i s3_bucket | cut -d" " -f 2)
 
-  cd _site
-
-  files=$(find . -type f -print)
-
-  for file in ${files}; do
-    modification_time=$(stat -c '%y' ${file})
-    expires_date=$(date -u -d "${modification_time} +1 year" '+%a, %d %b %Y %H:%M:%S GMT')
-    s3cmd --access_key=${AWS_KEY_ID} --secret_key=${AWS_SECRET_KEY} \
-    --add-header='Expires: ${expires_date}' \
-    _site/ s3://${s3_bucket}/
-  done
+  s3_bucket=$(cat s3_website.yml | grep -i s3_bucket | cut -d" " -f 2)
+  expires=$(date '+%a, %d %b %Y %H:%M:%S GMT' -d '+1 year')
+  s3cmd --access_key=${AWS_KEY_ID} --secret_key=${AWS_SECRET_KEY} \
+  --recursive modify --add-header="Expires: ${expires}" s3://${s3_bucket}/
 else
   echo 'Invalid branch. You can only deploy from master and stage.'
   exit 1
